1bbf0e1e20b34d5222b8a2d8d911a219bf0549f0
writeosahon <wrwiteosahon@yahoo.co.uk>
2018-12-02 20:18:59 +0100

    initial commit

--- www/js/compiled-js/controller-compiled.js
+++ www/js/compiled-js/controller-compiled.js
@@ -612,24 +612,20 @@ utopiasoftware[utopiasoftware_app_namespace].controller = {
                 case 0:
 
                     // reset the the segment button contained in the other carousel items to their initial state
-                    $("#login-page ons-carousel-item.second .login-segment button:nth-of-type(2) input").prop("checked", true);
-                    $("#login-page ons-carousel-item.second .login-segment button:nth-of-type(1) input").prop("checked", false);
-                    $("#login-page ons-carousel-item.third .login-segment button input").prop("checked", false);
+                    $("#login-page ons-carousel-item.second .login-segment").get(0).setActiveButton(1);
+                    $("#login-page ons-carousel-item.third .login-segment").get(0).setActiveButton(-1);
                     break;
 
                 case 1:
                     // reset the the segment button contained in the other carousel items to their initial state
-                    $("#login-page ons-carousel-item.first .login-segment button:nth-of-type(1) input").prop("checked", true);
-                    $("#login-page ons-carousel-item.first .login-segment button:nth-of-type(2) input").prop("checked", false);
-                    $("#login-page ons-carousel-item.third .login-segment button input").prop("checked", false);
+                    $("#login-page ons-carousel-item.first .login-segment").get(0).setActiveButton(0);
+                    $("#login-page ons-carousel-item.third .login-segment").get(0).setActiveButton(-1);
                     break;
 
                 case 2:
                     // reset the the segment button contained in the other carousel items to their initial state
-                    $("#login-page ons-carousel-item.first .login-segment button:nth-of-type(1) input").prop("checked", true);
-                    $("#login-page ons-carousel-item.first .login-segment button:nth-of-type(2) input").prop("checked", false);
-                    $("#login-page ons-carousel-item.second .login-segment button:nth-of-type(2) input").prop("checked", true);
-                    $("#login-page ons-carousel-item.second .login-segment button:nth-of-type(1) input").prop("checked", false);
+                    $("#login-page ons-carousel-item.first .login-segment").get(0).setActiveButton(0);
+                    $("#login-page ons-carousel-item.second .login-segment").get(0).setActiveButton(1);
                     break;
             }
         }
