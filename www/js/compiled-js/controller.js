3f8884999cd7a64909e381b59c1f95bae1dd53b1
writeosahon <wrwiteosahon@yahoo.co.uk>
2019-01-09 22:02:41 +0100

    initial commit

--- www/js/compiled-js/controller.js
+++ www/js/compiled-js/controller.js
@@ -4434,7 +4434,7 @@ utopiasoftware[utopiasoftware_app_namespace].controller = {
                         placeholder: ' ',
                         step: 1,
                         strictMode: true,
-                        width: '25%',
+                        width: '60%',
                         // sets value to the NumericTextBox
                         value: 1
                     }).appendTo($('#view-cart-page .view-cart-quantity-input').get(0));
