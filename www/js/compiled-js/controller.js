ff5c7dcc378073e455b099b3a21084c4fa1028ed
writeosahon <wrwiteosahon@yahoo.co.uk>
2019-01-13 12:30:40 +0100

    initial commit

--- www/js/compiled-js/controller.js
+++ www/js/compiled-js/controller.js
@@ -4439,9 +4439,6 @@ utopiasoftware[utopiasoftware_app_namespace].controller = {
             $('#customise-product-page #customise-product-add-to-cart').get(0).ej2_instances[0].dataBind();
             $('#customise-product-page #customise-product-add-to-cart').get(0).ej2_instances[0].start();
 
-            // show spinner to block customisation frame while it's loading
-            ej.popups.showSpinner($('#customise-product-page #customise-product-page-iframe-container').get(0));
-
             // call the method to submit the product customisation form located in the iframe window
             $('#customise-product-page #customise-product-page-iframe').get(0).contentWindow.utopiasoftware_addUsage();
         }
