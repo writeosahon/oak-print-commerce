ed5f7379f45b5a02515be34dd87ca18a15957172
writeosahon <wrwiteosahon@yahoo.co.uk>
2019-01-23 09:08:35 +0100

    initial commit

--- www/js/compiled-js/controller.js
+++ www/js/compiled-js/controller.js
@@ -6821,8 +6821,8 @@ utopiasoftware[utopiasoftware_app_namespace].controller = {
                 // update the select dropdownlist for country
                 countryDropDownList.value = (userDetails.billing && userDetails.billing.country && userDetails.billing.country !== "")
                     ? userDetails.billing.country : 'NG';
-                countryDropDownList.dataBind();
                 countryDropDownList.select = countrySelectListener; // reinstate the country select listener
+                countryDropDownList.dataBind();
                 // update the select dropdownlist for state
                 let statesDropDownList = $('#billing-info-page #billing-info-form #billing-info-state').get(0).ej2_instances[0];
                 statesDropDownList.dataSource = countryDropDownList.dataSource.find(function(countryElement){
