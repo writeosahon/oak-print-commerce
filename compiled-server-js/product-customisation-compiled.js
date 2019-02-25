47cd7b318db8c0bed53474a5ce627a821de69e96
writeosahon <wrwiteosahon@yahoo.co.uk>
2019-02-25 10:36:27 +0100

    initial commit

--- compiled-server-js/product-customisation-compiled.js
+++ compiled-server-js/product-customisation-compiled.js
@@ -1,3 +1,105 @@
-"use strict";var utopiasoftware_Cookies={};window.addEventListener("message",utopiasoftware_receiveMessage,!1),jQuery(document).ready(function(){if(window!==window.parent&&(jQuery("html").addClass("utopiasoftware-mobile"),utopiasoftware_Cookies=Cookies.noConflict()),!0===jQuery("html").hasClass("utopiasoftware-mobile"))return jQuery("html, body").scrollTop(0),window.parent.postMessage("page ready","*"),void window.setTimeout(utopiasoftware_getUsage,0)});function utopiasoftware_receiveMessage(){}function utopiasoftware_getUsage(){return new Promise(function(){Promise.resolve(jQuery.ajax({url:"https://shopoakexclusive.com/wp-json/wc/v2/cart",type:"get",contentType:"application/json",crossDomain:!0,xhrFields:{withCredentials:!0},dataType:"json",timeout:24e4,processData:!1})).then(function(a){window.parent.postMessage(JSON.stringify(a),"*")}).catch(function(a){console.log("SERVER ERROR",a)})})}function utopiasoftware_addUsage(){jQuery(".variations_form.cart button[type=\"submit\"].button").get(0).click()}function utopiasoftware_removeUsage(){var a=utopiasoftware_Cookies.get();for(var b in a)utopiasoftware_Cookies.remove(b);window.localStorage.clear(),window.sessionStorage.clear()}
+"use strict";
+
+var utopiasoftware_Cookies = {}; // holds the Global cookie object for the js-cookie library
+
+
+// register a listener for receiving messages from the mobile app
+window.addEventListener("message", utopiasoftware_receiveMessage, false);
+
+// wait for the entire document to be loaded
+jQuery(document).ready(function () {
+
+    // check if the site is being accessed from an iframe
+    if (window !== window.parent) {
+        // page is being accessed from an iframe
+        // add the utopiasoftware-mobile class to the HTML tag
+        jQuery('html').addClass("utopiasoftware-mobile");;
+
+        // set the use of the Cookie library
+        utopiasoftware_Cookies = Cookies.noConflict();
+    }
+
+    // check if the utopiasoftware-mobile class is set
+    if (jQuery('html').hasClass('utopiasoftware-mobile') === true) {
+        // class has been set
+
+        // scroll the window to the top
+        jQuery('html, body').scrollTop(0);
+
+        // post message to app that site is loaded
+        window.parent.postMessage("page ready", "*");
+
+        // check the user cart in a different event block
+        window.setTimeout(utopiasoftware_getUsage, 0);
+        return;
+    }
+});
+
+/**
+ * method is used to handle the receipt of messages from the mobile app
+ *
+ * @param receiveEvent {Event} this is the event object of the "Message" event
+ *
+ */
+function utopiasoftware_receiveMessage(receiveEvent) {}
+
+/**
+ * method is used to get the currently logged in user's cart
+ *
+ * @returns {Promise<any>}
+ */
+function utopiasoftware_getUsage() {
+
+    // return a Promise object which resolves when the process of setting usage is done
+    return new Promise(function (resolve, reject) {
+        Promise.resolve(jQuery.ajax({
+            url: "https://shopoakexclusive.com/wp-json/wc/v2/cart",
+            type: "get",
+            contentType: "application/json",
+            crossDomain: true,
+            xhrFields: {
+                withCredentials: true
+            },
+            dataType: "json",
+            timeout: 240000, // wait for 4 minutes before timeout of request
+            processData: false
+        })).then(function (serverData) {
+            // post message to mobile app containing the current cart in session
+            window.parent.postMessage(JSON.stringify(serverData), "*");
+        }).catch(function (err) {
+            console.log("SERVER ERROR", err);
+        });
+    });
+}
+
+/**
+ *
+ * method is used to add the ('presently') current product being customised to the available cart
+ */
+function utopiasoftware_addUsage() {
+    // trigger the submission of the customisation form by triggering a click on the submit button
+    jQuery('.variations_form.cart button[type="submit"].button').get(0).click();
+}
+
+/**
+ * method is used to remove all cookies, local storage and
+ * sessions storage used to keep access to the user's cart. i.e. it is used to clear the user's cart
+ */
+function utopiasoftware_removeUsage() {
+
+    // get all the cookies created by this site
+    var localCookies = utopiasoftware_Cookies.get();
+
+    // remove each cookie using a for-loop
+    for (var item in localCookies) {
+        utopiasoftware_Cookies.remove(item); // remove the cookie
+    }
+
+    // remove the localStorage data
+    window.localStorage.clear();
+
+    // remove the sessionStorage data
+    window.sessionStorage.clear();
+}
 
 //# sourceMappingURL=product-customisation-compiled.js.map
