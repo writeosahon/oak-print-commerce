5e85055c4005e6fcdc5d812baf8a8fe411ab552a
writeosahon <wrwiteosahon@yahoo.co.uk>
2019-03-11 15:18:44 +0100

    initial commit

--- compiled-server-js/edit-page-compiled.js
+++ compiled-server-js/edit-page-compiled.js
@@ -1,3 +1,3 @@
-'use strict';jQuery(document).ready(function(){jQuery('.woocommerce-message').append('<a href="/shop/" tabindex="2" class="button wc-forward">Continue shopping</a>')});
+'use strict';jQuery(document).ready(function(){jQuery('.woocommerce-message').append('<a href="/shop/" tabindex="2" class="button wc-forward">Continue shopping</a>'),jQuery('#oak-exclusive-copyright-date').html(''+new Date().getFullYear())});
 
 //# sourceMappingURL=edit-page-compiled.js.map
--- compiled-server-js/edit-page.js
+++ compiled-server-js/edit-page.js
@@ -2,7 +2,9 @@
 /**
  * this script is used to dynamically add the "Continue Shopping" button to the
  * website individual Product page. The button is displayed after a product
- * has been successfully added to the user's cart
+ * has been successfully added to the user's cart.
+ *
+ * The page also dynamically updates the copyright year contented in the Footer section of every page
  **/
 
 // wait for the entire document to be loaded
@@ -11,4 +13,7 @@ jQuery(document).ready(function(){
     // add the "Continue Shopping" button
     jQuery('.woocommerce-message').
     append(`<a href="/shop/" tabindex="2" class="button wc-forward">Continue shopping</a>`);
+
+    // add the copyright year
+    jQuery('#oak-exclusive-copyright-date').html("" + new Date().getFullYear());
 });
